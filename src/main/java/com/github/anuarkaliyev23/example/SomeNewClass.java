3f6de39c04fe540f97db92b7fba4ad4452235cc4
Khambar <anuarkaliyev23@gmail.com>
2020-03-19 18:07:59 +0600

    Some changes I've made

---
+++ src/main/java/com/github/anuarkaliyev23/example/SomeNewClass.java
@@ -0,0 +1,13 @@
+package com.github.anuarkaliyev23.example;
+
+public class SomeNewClass {
+    public int getSomething() {
+        return something;
+    }
+
+    public void setSomething(int something) {
+        this.something = something;
+    }
+
+    private int something;
+}
